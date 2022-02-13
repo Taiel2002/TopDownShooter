6458a715ef8fcae0e030804a04cb8e83a76b8b40
Taiel2002 <tengominecraftblog@gmail.com>
2022-02-12 21:29:11 -0300

    bullet passing through bug fixed

--- Top Down Shooter/Assets/Guns/Bullet/BulletMovement.cs
+++ Top Down Shooter/Assets/Guns/Bullet/BulletMovement.cs
@@ -8,6 +8,14 @@ public class BulletMovement : MonoBehaviour
     public float speed;
     public float lifeTime;
     [SerializeField] private Transform collisionVFX;
+    public Vector3 origin;
+    RaycastHit hit;
+
+    private void Awake()
+    {
+        origin = transform.position;
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,13 +23,25 @@ public class BulletMovement : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
         StartCoroutine(DestroyBullet());
     }
-
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
         Vector3 rotation = new Vector3(transform.rotation.x, transform.rotation.y - 180, transform.rotation.z);
         Instantiate(collisionVFX, transform.position, Quaternion.Euler(rotation)); ;
         Destroy(this.gameObject);
     }
+
+    //private void FixedUpdate()
+    //{
+    //    if (Physics.Linecast(transform.position, (origin - transform.position), out hit))
+    //    {
+    //        if (hit.transform.tag != "Bullet")
+    //        {
+    //            Vector3 rotation = new Vector3(transform.rotation.x, transform.rotation.y - 180, transform.rotation.z);
+    //            Instantiate(collisionVFX, transform.position, Quaternion.Euler(rotation)); ;
+    //            Destroy(this.gameObject);
+    //        }            
+    //    }
+    //}
 
     IEnumerator DestroyBullet()
     {
